73fc8bc07b9457fe90dcb664eeadbc30839e4d98
506108-stack <506108@daimon-h.ed.jp>
2026-07-17 08:59:40 +0900

    Update face.js

--- face.js
+++ face.js
@@ -1,1 +1,16 @@
+const video = document.getElementById("webcam");
 
+async function startCamera(){
+
+const stream =
+await navigator.mediaDevices.getUserMedia({
+
+video:true
+
+});
+
+video.srcObject = stream;
+
+}
+
+startCamera();
