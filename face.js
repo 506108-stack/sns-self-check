8e0ffd92e1d5a0127e8d814b8fe623ff76bc9cd2
506108-stack <506108@daimon-h.ed.jp>
2026-07-17 13:11:41 +0900

    Update face.js

--- face.js
+++ face.js
@@ -1,23 +1,6 @@
-// Face Landmarkerはここから作っていく！
-console.log("face.js 読み込み成功！");
+import {
+  FaceLandmarker,
+  FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/vision_bundle.mjs";
 
-const video = document.getElementById("webcam");
-const canvas = document.getElementById("output_canvas");
-
-console.log("video:", video);
-console.log("canvas:", canvas);
-
-async function startCamera() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true
-    });
-
-    video.srcObject = stream;
-    console.log("✅ カメラ起動成功！");
-  } catch (error) {
-    console.error("❌ カメラ起動失敗", error);
-  }
-}
-
-startCamera();
+console.log("✅ MediaPipe読み込み成功");
