5780cab4151e719e3bc147247eadb7bc1290446a
506108-stack <506108@daimon-h.ed.jp>
2026-07-17 12:53:50 +0900

    Update face.js

--- face.js
+++ face.js
@@ -1,2 +1,8 @@
 // Face Landmarkerはここから作っていく！
 console.log("face.js 読み込み成功！");
+const video = document.getElementById("webcam");
+const canvas = document.getElementById("output_canvas");
+const ctx = canvas.getContext("2d");
+
+console.log(video);
+console.log(canvas);
