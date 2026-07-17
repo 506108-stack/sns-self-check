1bfd1a77aec5ac20db591a299aed4db885d4cb0d
506108-stack <506108@daimon-h.ed.jp>
2026-07-17 13:07:56 +0900

    Update face.js

--- face.js
+++ face.js
@@ -1,17 +1,23 @@
 // Face Landmarkerはここから作っていく！
 console.log("face.js 読み込み成功！");
+
 const video = document.getElementById("webcam");
 const canvas = document.getElementById("output_canvas");
-const ctx = canvas.getContext("2d");
-// カメラを起動
-async function startCamera() {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true
-  });
 
-  video.srcObject = stream;
+console.log("video:", video);
+console.log("canvas:", canvas);
+
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true
+    });
+
+    video.srcObject = stream;
+    console.log("✅ カメラ起動成功！");
+  } catch (error) {
+    console.error("❌ カメラ起動失敗", error);
+  }
 }
 
 startCamera();
-console.log(video);
-console.log(canvas);
