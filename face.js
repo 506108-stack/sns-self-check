9e1564c7c52d3c9a5fcda1c41a053b694fe7ef73
506108-stack <506108@daimon-h.ed.jp>
2026-07-18 16:44:30 +0900

    Update face.js

--- face.js
+++ face.js
@@ -4,12 +4,13 @@ import {
 } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/vision_bundle.mjs";
 
 console.log("✅ MediaPipe読み込み成功");
-// MediaPipeの準備
+
 const vision = await FilesetResolver.forVisionTasks(
   "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
 );
 
 console.log("✅ Vision準備完了");
+
 const video = document.getElementById("webcam");
 
 async function startCamera() {
@@ -19,8 +20,11 @@ async function startCamera() {
   });
 
   video.srcObject = stream;
+
+  // ★追加
+  await video.play();
+
+  console.log("動画サイズ", video.videoWidth, video.videoHeight);
 }
 
 startCamera();
-console.log(video);
-console.log(video.srcObject);
