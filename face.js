1ec176e841e42290866d21b243f65cebc5b7849f
506108-stack <506108@daimon-h.ed.jp>
2026-07-19 11:02:45 +0900

    Update face.js

--- face.js
+++ face.js
@@ -6,7 +6,7 @@ import {
 console.log("✅ MediaPipe読み込み成功");
 
 const vision = await FilesetResolver.forVisionTasks(
-  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
 );
 
 console.log("✅ Vision準備完了");
